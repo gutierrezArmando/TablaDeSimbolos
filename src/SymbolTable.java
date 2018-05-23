38fb82eeaf0be6c7c43259a5c32e22349becedf1
Armando Gutiérrez <gutierrezjarr@gmail.com>
2018-05-22 23:40:49 -0800

    Initial commit

---
+++ src/SymbolTable.java
@@ -0,0 +1,44 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class SymbolTable implements Scope {
+	
+	Map<String , Symbol> symbols = new HashMap<String , Symbol>();
+	
+	public SymbolTable()
+	{
+		intTypeSystem();
+	}
+	
+	protected void intTypeSystem()
+	{
+		define(new BuiltInTypeSymbol("int"));
+		define(new BuiltInTypeSymbol("float"));
+	}
+	
+	public String getScopeName()
+	{
+		return "global";
+	}
+	
+	public Scope getEnclosingScope()
+	{
+		return null;
+	}
+	
+	public void define(Symbol sym)
+	{
+		symbols.put(sym.name,sym);
+	}
+	
+	public Symbol resolve(String name)
+	{
+		return symbols.get(name);
+	}
+	
+	public String toString()
+	{
+		return getScopeName() + ":" + symbols;
+	}
+	
+}
